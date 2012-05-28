8a6db86e93264cc710d5d457e4e3ce59b365d7d4
Patrick McElwee <pmcelwee@gmail.com>
2012-05-28 14:49:06 -0400

    Reverted temporary deletion of ContentImages and ContentFiles resources

---
+++ app/controllers/content_images_controller.rb
@@ -0,0 +1,9 @@
+class ContentImagesController < Spree::BaseController
+  def create
+    @content_image = ContentImage.new(params[:content_image])
+    @content_image.save!
+
+    render :text => @content_image.attachment.url
+  end
+
+en
